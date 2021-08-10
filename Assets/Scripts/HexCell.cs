c59f731e076b2e35b4dd3912a526411c881407a5
Skyliar <skyspree1@gmail.com>
2021-08-10 17:27:01 -0500

    catLikeCodingHexMap1AndStartOfHexMap2
    completed cat like coding Hex Map 1 and started a little in Hex map 2

---
+++ Assets/Scripts/HexCell.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class HexCell : MonoBehaviour
+{
+	#region Fields
+	
+	public HexCoordinates coordinates;
+	public Color color;
+
+	[SerializeField]
+	HexCell[] neighbors;
+
+	#endregion
+
+	#region Properties
+
+	#region GetNeighbor
+	public HexCell GetNeighbor(HexDirection direction)
+	{
+		return neighbors[(int)direction];
+	}
+	#endregion
+
+	#region SetNeighbor
+	public void SetNeighbor(HexDirection direction, HexCell cell)
+	{
+		neighbors[(int)direction] = cell;
+		cell.neighbors[(int)direction.Opposite()] = this;
+	}
+	#endregion
+
+	#endregion
+
+}
