a60520394eaf42ebc1b623880a489d8699855d8f
Skyliar <skyspree1@gmail.com>
2022-09-26 20:29:06 -0500

    catLikeCoding HexMap part 7.3

--- Assets/Scripts/HexCell.cs
+++ Assets/Scripts/HexCell.cs
@@ -7,6 +7,7 @@ public class HexCell : MonoBehaviour
 	#region Fields
 	
 	public HexCoordinates coordinates;
+
 	public Color Color {
 		get
 		{
@@ -59,9 +60,18 @@ public class HexCell : MonoBehaviour
 				RemoveIncomingRiver();
 			}
 
+			for (int i = 0; i < roads.Length; i++)
+			{
+				if (roads[i] && GetElevationDifference ((HexDirection)i) > 1)
+				{
+					SetRoad(i, false);
+				}
+			}
+
 			Refresh();
 		}
 	}
+
 	int elevation = int.MinValue;
 
 	public RectTransform uiRect;
@@ -73,6 +83,9 @@ public class HexCell : MonoBehaviour
 
 	HexDirection incomingRiver;
 	HexDirection outgoingRiver;
+
+	[SerializeField]
+	bool[] roads;
 
 	#endregion
 
@@ -144,6 +157,8 @@ public class HexCell : MonoBehaviour
 		chunk.Refresh();
 	}
 	#endregion
+
+	#region Rivers
 
 	public bool HasIncomingRiver
 	{
@@ -227,6 +242,7 @@ public class HexCell : MonoBehaviour
 		neighbor.hasOutgoingRiver = false;
 		neighbor.RefreshSelfOnly();
 	}
+
 	public void RemoveRiver()
 	{
 		RemoveOutgoingRiver();
@@ -254,12 +270,12 @@ public class HexCell : MonoBehaviour
 
 		hasOutgoingRiver = true;
 		outgoingRiver = direction;
-		RefreshSelfOnly();
 
 		neighbor.RemoveIncomingRiver();
 		neighbor.hasIncomingRiver = true;
 		neighbor.incomingRiver = direction.Opposite();
-		neighbor.RefreshSelfOnly();
+
+		SetRoad((int)direction, false);
 	}
 
 	public float StreamBedY
@@ -277,6 +293,65 @@ public class HexCell : MonoBehaviour
 			return (elevation + HexMetrics.riverSurfaceElevationOffset) * HexMetrics.elevationStep;
 		}
 	}
+
+	#endregion
+
+	#region Roads
+	public bool HasRoadThroughEdge (HexDirection direction)
+	{
+		return roads[(int)direction];
+	}
+
+	public bool HasRoads
+	{
+		get
+		{
+			for (int i = 0; i < roads.Length; i++)
+			{
+				if (roads[i])
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+
+	public void AddRoad(HexDirection direction)
+	{
+		if (!roads[(int)direction] && !HasRiverThroughEdge(direction) &&
+			GetElevationDifference(direction) <= 1)
+		{
+			SetRoad((int)direction, true);
+		}
+	}
+
+	public void RemoveRoads()
+	{
+		for (int i = 0; i < neighbors.Length; i++)
+		{
+			if (roads[i])
+			{
+				SetRoad(i, false);
+			}
+		}
+	}
+
+	void SetRoad(int index, bool state)
+	{
+		roads[index] = state;
+		neighbors[index].roads[(int)((HexDirection)index).Opposite()] = state;
+		neighbors[index].RefreshSelfOnly();
+		RefreshSelfOnly();
+	}
+
+	public int GetElevationDifference (HexDirection direction)
+	{
+		int difference = elevation - GetNeighbor(direction).elevation;
+		return difference >= 0 ? difference : -difference;
+	}
+
+	#endregion
 
 	#endregion
 
