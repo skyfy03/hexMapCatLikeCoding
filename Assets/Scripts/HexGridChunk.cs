ac6b5e46f9677be0b16c547479e001182735a5d8
Skyliar <skyspree1@gmail.com>
2022-09-28 12:00:58 -0500

    catLikeCoding Hex Map 8 6.1 Hiding UnderWater Rivers Complete

--- Assets/Scripts/HexGridChunk.cs
+++ Assets/Scripts/HexGridChunk.cs
@@ -287,9 +287,12 @@ public class HexGridChunk : MonoBehaviour
 		terrain.AddTriangle(centerR, m.v4, m.v5);
 		terrain.AddTriangleColor(cell.Color);
 
-		bool reversed = cell.IncomingRiver == direction;
-		TriangulateRiverQuad(centerL, centerR, m.v2, m.v4, cell.RiverSurfaceY, 0.4f, reversed);
-		TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.6f, reversed);
+		if (!cell.IsUnderwater)
+		{
+			bool reversed = cell.IncomingRiver == direction;
+			TriangulateRiverQuad(centerL, centerR, m.v2, m.v4, cell.RiverSurfaceY, 0.4f, reversed);
+			TriangulateRiverQuad(m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.6f, reversed);
+		}
 	}
 
 	void TriangulateWithRiverBeginOrEnd(HexDirection direction, HexCell cell, Vector3 center, EdgeVertices e)
@@ -301,24 +304,27 @@ public class HexGridChunk : MonoBehaviour
 		TriangulateEdgeStrip(m, cell.Color, e, cell.Color);
 		TriangulateEdgeFan(center, m, cell.Color);
 
-		bool reversed = cell.HasIncomingRiver;
-		TriangulateRiverQuad(
-			m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.6f, reversed
-		);
+		if (!cell.IsUnderwater)
+		{
+			bool reversed = cell.HasIncomingRiver;
+			TriangulateRiverQuad(
+				m.v2, m.v4, e.v2, e.v4, cell.RiverSurfaceY, 0.6f, reversed
+			);
 
-		center.y = m.v2.y = m.v4.y = cell.RiverSurfaceY;
-		rivers.AddTriangle(center, m.v2, m.v4);
-		if (reversed)
-		{
-			rivers.AddTriangleUV(
-				new Vector2(0.5f, 0.4f), new Vector2(1f, 0.2f), new Vector2(0f, 0.2f)
-			);
-		}
-		else
-		{
-			rivers.AddTriangleUV(
-				new Vector2(0.5f, 0.4f), new Vector2(0f, 0.6f), new Vector2(1f, 0.6f)
-			);
+			center.y = m.v2.y = m.v4.y = cell.RiverSurfaceY;
+			rivers.AddTriangle(center, m.v2, m.v4);
+			if (reversed)
+			{
+				rivers.AddTriangleUV(
+					new Vector2(0.5f, 0.4f), new Vector2(1f, 0.2f), new Vector2(0f, 0.2f)
+				);
+			}
+			else
+			{
+				rivers.AddTriangleUV(
+					new Vector2(0.5f, 0.4f), new Vector2(0f, 0.6f), new Vector2(1f, 0.6f)
+				);
+			}
 		}
 	}
 
@@ -357,9 +363,12 @@ public class HexGridChunk : MonoBehaviour
 		if (cell.HasRiverThroughEdge(direction))
 		{
 			e2.v3.y = neighbor.StreamBedY;
-			TriangulateRiverQuad(e1.v2, e1.v4, e2.v2, e2.v4, cell.RiverSurfaceY, neighbor.RiverSurfaceY, 0.8f,
-				cell.HasIncomingRiver && cell.IncomingRiver == direction
-			);
+			if (!cell.IsUnderwater && !neighbor.IsUnderwater)
+			{
+				TriangulateRiverQuad(e1.v2, e1.v4, e2.v2, e2.v4, cell.RiverSurfaceY, neighbor.RiverSurfaceY, 0.8f,
+					cell.HasIncomingRiver && cell.IncomingRiver == direction
+				);
+			}
 		}
 
 		if (cell.GetEdgeType(direction) == HexEdgeType.Slope)
